aa236e7e3426d188d7f560db347d42a17b63e0d5
Simonc-TCHEDJI <tchedji.simonck@gmail.com>
2024-06-19 21:27:19 +0100

    Flow du premier contact mis en place

---
+++ assets/js/primo.js
@@ -0,0 +1,22 @@
+let addCompteAction = document.querySelector(".main .page .container .box");
+let allPage = document.querySelectorAll(".main .page");
+let addComptePage = document.querySelector(".main .page.addCompte");
+let pageOtp = document.querySelector(".main .page.setOtp");
+let pageChoix = document.querySelector(".main .page.choix");
+
+
+addCompteAction.addEventListener("click", (e) => {
+    e.preventDefault();
+    changePage(allPage, addComptePage, "active");
+});
+
+let changePage = (pages, pageGoTo, classe) => {
+    RemoteAllClass(pages, classe);
+    pageGoTo.classList.add(classe);
+};
+
+let RemoteAllClass = (htmlArray, classe) => {
+    htmlArray.forEach(element => {
+        element.classList.remove(classe);
+    });
+};
